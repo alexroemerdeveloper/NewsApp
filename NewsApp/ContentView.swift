bf303940d747ac8ec63cf2e665a742174ce6a9b4
Alexander Römer <alex@roemer.design>
2019-12-30 19:10:17 +0100

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by Alexander Römer on 30.12.19.
+//  Copyright © 2019 Alexander Römer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
